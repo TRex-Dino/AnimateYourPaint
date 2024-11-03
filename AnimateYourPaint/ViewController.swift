860dc2b9587506b794c162691a88df3c37361585
menkov-dmitry <dmitry.menkov@vk.team>
2024-11-04 01:56:00 +0300

    create project

---
+++ AnimateYourPaint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimateYourPaint
+//
+//  Created by Dmitry Menkov on 04.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
